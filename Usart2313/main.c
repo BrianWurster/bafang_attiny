27cb6792fa35a9198dc4466c9868700b53de2e92
Brian Wurster <bwurster@gmail.com>
2018-09-21 14:29:01 -0400

    initial project and F_CPU

---
+++ Usart2313/main.c
@@ -0,0 +1,18 @@
+/*
+ * Usart2313.c
+ *
+ * Created: 9/21/2018 2:23:59 PM
+ * Author : brianw
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
